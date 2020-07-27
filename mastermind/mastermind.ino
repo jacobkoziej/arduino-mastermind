33d7fd1322a8dcd006ac0a73d9023cb6c8572d31
Jacob Koziej <jacobkoziej@gmail.com>
2020-07-27 16:18:28 -0400

    Declare base sketch functions

--- mastermind/mastermind.ino
+++ mastermind/mastermind.ino
@@ -15,3 +15,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+void setup()
+{
+}
+
+void loop()
+{
+}
